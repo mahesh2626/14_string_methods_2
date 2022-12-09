42e1cf988ed797ae21cdeb957068262ca1c6d980
mahesh2626 <118418933+mahesh2626@users.noreply.github.com>
2022-12-09 12:05:40 +0530

    Add files via upload

---
+++ string methods part2.py
@@ -0,0 +1,152 @@
+# t='hEllo pyThon'
+# print(t.title())
+# print(t.capitalize())
+# print(t.istitle())
+# print(t.isupper())
+# print(t.islower())
+
+# a="Hello Python"
+# print(a.istitle())
+# print(a.capitalize())
+
+# a="Hello Python"
+# print(a.endswith(""))
+# print(a.endswith(' '))
+# print(a.endswith('n'))
+
+# a="Hello Python"
+# print(a.startswith(''))
+# print(a.startswith(' '))
+# print(a.startswith('h'))
+# print(a.startswith('H'))
+
+
+# z=' hEllo pyThon'
+# print(z.startswith(''))
+# print(z.startswith(' '))
+# print(z.startswith('h'))
+# print(z.startswith(' h'))
+
+# x=''
+# v="  "
+# print(x.isspace())
+# print(v.isspace())
+
+# x="hello python programming"
+# print(x.index('h'))
+# print(x.index('m'))
+# print(x.rindex("g"))
+# print(x.index("g"))
+# print(x.rindex('h'))
+# print(x.rindex('m'))
+# print(len("hello python programming"))
+
+# x="hello python programming"
+# print(x.find('p'))
+# print(x.rfind('p'))
+# print(x.find('i'))
+# print(len(x))
+# print(x.rfind('h'))
+
+# x="hello python programming"
+# print(x.count('l'))
+# print(x.count('p'))
+# print(x.count('e'))
+# print(x.count('g'))
+# print(x.count('f'))
+
+# x="hello python programming"
+# print(x.replace("hello","hi"))
+# print(x.replace('h','2'))
+# print(x.replace('h','r',1))
+# print(x.replace('o','u',4))
+# print(x.replace('m',' ',1))
+
+# x="     hellopython   programming  ,  "
+# print(x.strip())
+# print(x.lstrip())
+# print(x.rstrip())
+# v="   hellopythonprogramming   "
+# print(v.strip())
+# print(v.rstrip())
+# print(v.lstrip())
+
+# x="hello"," python"," programming"
+# v=" world".join(x)
+# print(v)
+# a='hello'
+# x='*'.join(a)
+# print(x)
+# v='hey',
+# b="%".join(v)
+# print(b)
+
+# x="hello python programming"
+# print(x.removeprefix('h'))
+# print(x.removeprefix('H'))
+# print(x.removeprefix('he'))
+# print(x.removeprefix('ell'))
+
+# print(x.removesuffix('g'))
+# print(x.removesuffix('ng'))
+# print(x.removesuffix('mm'))
+# print(x.removesuffix('programming'))
+
+# x="hello python programming"
+# print(x.isalnum())
+
+# x="hellopythonprogramming"
+# print(x.isalnum())
+# a="hello2python55335programming"
+# print(a.isalnum())
+
+# x="hello python programming"
+# print(x.isalpha())
+# x="hellopythonprogramming"
+# print(x.isalpha())
+# x="26hellopython2programming"
+# print(x.isalpha())
+
+# x="hello python programming"
+# print(x.isascii())
+
+# print(chr(0))
+# print(chr(32))
+# print(chr(0)+'hey')
+# print(chr(32)+'hey')
+
+# print(ord('a'))
+# print(ord('A'))
+# print(ord('z'))
+# print(ord('Z'))
+# print(ord('!'))
+# print(ord('9'))
+# print(chr(97))
+# print(chr(122))
+# print(chr(65))
+
+
+# a=bin(55)
+# print(a,type('a'))
+# print(bin(66))
+# print(0b110)
+
+# v=oct(656)
+# print(v)
+# print(oct(64))
+# print(0o0101)
+
+# a=hex(897)
+# print(a)
+# print(0x10121)
+# print(hex(16))
+
+# r="helly hello"
+# a=r.maketrans("l",'r')
+# print(a,type(a))
+# print(r.translate(a))
+# b=r.replace('l','r')
+# print(b)
+
+# print(ord('l'))
+# print(ord('r'))
